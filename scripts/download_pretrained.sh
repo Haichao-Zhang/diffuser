c5131691b507bec4034e7f3b8d5daca4907cbdf8
Haichao Zhang <hczhang1@gmail.com>
2023-06-24 00:34:17 -0700

    Fix download script

--- scripts/download_pretrained.sh
+++ scripts/download_pretrained.sh
@@ -1,4 +1,4 @@
 mkdir -p logs
 gdown https://drive.google.com/uc?id=1wc1m4HLj7btaYDN8ogDIAV9QzgWEckGy
-tar -xvf pretrained.tar --directory logs
-rm pretrained.tar
+tar -xvf pretrained-v0.tar --directory logs
+rm pretrained-v0.tar
